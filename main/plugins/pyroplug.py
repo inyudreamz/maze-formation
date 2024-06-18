892885c9fb0daef760c9db8f7e86e0dad45aa1a8
inyudreamz <inyu.dreamz@gmail.com>
2024-06-18 22:56:04 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -257,7 +257,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     logging.info(e)
                     thumb_path = None
                 
-                caption = msg.caption if msg.caption else file_n
+                caption = msg.caption if msg.caption else path
                 await send_video_with_chat_id(client, sender, path, caption, duration, hi, wi, thumb_path, upm)
             elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
                 if file_n != '':
@@ -271,7 +271,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
 
                 
-                caption = msg.caption if msg.caption else file_n
+                caption = msg.caption if msg.caption else path
                 await upm.edit("__Uploading photo...__")
 
                 await bot.send_file(sender, path, caption=caption)
@@ -287,7 +287,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
                 thumb_path = "thumb.jpg"
                 
-                caption = msg.caption if msg.caption else file_n
+                caption = msg.caption if msg.caption else path
                 await send_document_with_chat_id(client, sender, path, caption, thumb_path, upm)
             os.remove(file)
             await upm.delete()
@@ -426,7 +426,7 @@ async def ggn_new(userbot, client, sender, edit_id, msg_link, i, file_n):
                     logging.info(e)
                     thumb_path = None
                 
-                caption = msg.caption if msg.caption else file_n
+                caption = msg.caption if msg.caption else path
                 await send_video_with_chat_id(client, sender, path, caption, duration, hi, wi, thumb_path, upm)
             elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
                 if file_n != '':
@@ -440,7 +440,7 @@ async def ggn_new(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
 
                 
-                caption = msg.caption if msg.caption else file_n
+                caption = msg.caption if msg.caption else path
                 await upm.edit("__Uploading photo...__")
 
                 await bot.send_file(sender, path, caption=caption)
@@ -456,7 +456,7 @@ async def ggn_new(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
                 thumb_path = "thumb.jpg"
                 
-                caption = msg.caption if msg.caption else file_n
+                caption = msg.caption if msg.caption else path
                 await send_document_with_chat_id(client, sender, path, caption, thumb_path, upm)
             os.remove(file)
             await upm.delete()
